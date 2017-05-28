2f3c1f57cf0df648855e7676d5e91e7162d6fdb1
Ryan <ryanc118@gmail.com>
2017-05-28 14:17:41 +0100

    Can source paths from any directory

--- paths.sh
+++ paths.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CODEBASE_ROOT=$(dirname "$0")
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$(pwd)/lib/"
-export PYTHONPATH=$PYTHONPATH:"$CODEBASE_ROOT/src/py_utils/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DIR/lib/"
+export PYTHONPATH="$PYTHONPATH:$DIR/src/py_utils/"
 
