f5a1bcffd40dc1d1b55115256bb96eda7c702c14
Ryan Collingham <ryanc118@gmail.com>
2017-07-06 22:14:18 +0100

    Optimizations and tidy ups for P68

--- src/problems/P68.py
+++ src/problems/P68.py
@@ -5,13 +5,19 @@ from itertools import permutations, chain
 from custom_itertools import iter_ordered_pairs
 
 N = 5
-
+MUST_BE_IN_EDGE = 10
 
 class NGon(object):
+    """Represents a 'magic' NGon - a collection of overlapping triplets of
+    numbers that all sum to the same total. An Ngon can be uniquely
+    specified by n pairs of non-repeating numbers."""
     def __init__(self, n, values):
         self.n = n
         self.values = [[None, None, None] for _ in range(n)]
         self.fill_values(values)
+
+    def __str__(self):
+        return ''.join(str(x) for x in chain.from_iterable(self.values))
 
     def verify(self):
         """Verify that the values form a self-consistent solution"""
@@ -25,10 +31,9 @@ class NGon(object):
 
         return True
 
-    def __str__(self):
-        return str(self.values)
-
     def fill_values(self, values):
+        """Take an input list of value pairs and generate a magic Ngon,
+        filling the overlapping values in each triplet."""
         # First rotate the values list until we have the lowest edge value at
         # the start.
         min_edge_val = (2 * self.n) + 1
@@ -39,6 +44,7 @@ class NGon(object):
 
         values = values[min_edge_ix:] + values[:min_edge_ix]
 
+        # Now fill in the overlapping third element of each triplet
         for i in range(self.n):
             for j in range(2):
                 self.values[i][j] = values[i][j]
@@ -46,6 +52,9 @@ class NGon(object):
 
 
 def main():
+    """Generate all possible Ngon solutions. Of those that verify as correct,
+    convert to strings of digits. Print the numerically largest such digit
+    string."""
     ngon_list = []
     ngon_int_list = []
 
@@ -55,24 +64,28 @@ def main():
             ngon_list.append(ngon)
 
     for ngon in ngon_list:
-        ngon_str = concat_to_str(ngon)
-        if len(ngon_str) == 16:
-            ngon_int_list.append(int(ngon_str))
+        ngon_str = str(ngon)
+        assert len(ngon_str) == 16
+        ngon_int_list.append(int(ngon_str))
 
     print("Max ngon integer is {}".format(max(ngon_int_list)))
 
 
 def iter_values(vals):
+    """Iterates through all possible unique orderings of pairings for a list
+    of values. Ignores orderings that are cyclically equivalent."""
     num_pairs = len(vals) // 2
 
     for pairing in iter_ordered_pairs(list(vals)):
         for p in permutations(pairing[1:]):
-            yield [pairing[0]] + list(p)
+            value_list = [pairing[0]] + list(p)
 
-
-def concat_to_str(ngon):
-    return ''.join(str(x) for x in chain.from_iterable(ngon.values))
-
+            # Optimization: only yield value lists if 10 is the first value
+            # in a pairing, as only these will give 16-digit strings.
+            for i in range(num_pairs):
+                if value_list[i][0] == MUST_BE_IN_EDGE:
+                    yield value_list
+                    break
 
 
 if __name__ == '__main__':
--- src/py_utils/custom_itertools.py
+++ src/py_utils/custom_itertools.py
@@ -5,7 +5,10 @@ import itertools
 
 
 def iter_pairs(l):
-    """Iterate through all unique pairings of iterable l."""
+    """Iterate through all unique pairings of iterable l. The order of each
+    pair does not matter (i.e. (1, 2) is equivalent to (2, 1) and the order
+    of each pair in the overall pairing does not matter (i.e. [(1, 2), (3, 4)]
+    is equivalent to [(3, 4), (1, 2)]."""
     num_el = len(l)
     assert num_el % 2 == 0
 
@@ -19,25 +22,30 @@ def iter_pairs(l):
             yield [pair] + rest
 
 def iter_ordered_pairs(l):
+    """Iterate through all possible full pairings of elements in l.
+    The order of each pairing matters (i.e. (1, 2) different from (2, 1)
+    but the order of the pairs in each pairing does not (i.e. [(1, 2), (3, 4)]
+    is equivalent to [(3, 4), (1, 2)]."""
+    def iter_pairing(pairs):
+        """Iterate through all pairings and generate all possible swaps of the
+        unordered pairs."""
+        num_pairs = len(pairs)
+        for i in range(num_pairs + 1):
+            for indices in itertools.combinations(range(num_pairs), i):
+                yield swap_pairs(pairs, indices)
+
+    def swap_pairs(pairs, indices):
+        """Swap pairs of elements at given indices."""
+        new_pairs = pairs
+
+        for ix in indices:
+            new_pairs[ix] = pairs[ix][::-1]
+
+        return new_pairs
+
     for pairing in iter_pairs(l):
         for ordered_pairing in iter_pairing(pairing):
             yield ordered_pairing
-
-
-def iter_pairing(pairs):
-    num_pairs = len(pairs)
-    for i in range(num_pairs + 1):
-        for indices in itertools.combinations(range(num_pairs), i):
-            yield swap_pairs(pairs, indices)
-
-
-def swap_pairs(pairs, indices):
-    new_pairs = pairs
-
-    for ix in indices:
-        new_pairs[ix] = pairs[ix][::-1]
-
-    return new_pairs
 
 
 if __name__ == '__main__':
