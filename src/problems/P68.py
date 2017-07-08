1e96b2f2d6a2bb5f331397f637fa6ee3ddcef0f7
Ryan <ryanc118@gmail.com>
2017-07-08 14:50:43 +0100

    Re-did P68 with faster algorithm

--- src/problems/P68.py
+++ src/problems/P68.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-"""P68 - Magic N-gons"""
 
 from itertools import permutations, chain
-from custom_itertools import iter_ordered_pairs
 
-N = 5
-MUST_BE_IN_EDGE = 10
+N = 5 
+TARGET_LEN = 16
+
 
 class NGon(object):
     """Represents a 'magic' NGon - a collection of overlapping triplets of
     numbers that all sum to the same total. An Ngon can be uniquely
     specified by n pairs of non-repeating numbers."""
-    def __init__(self, n, values):
-        self.n = n
-        self.values = values
-        self.valid = self._verify()
+    def __init__(self, values):
+        self.n = len(values) 
+        self._values = tuple(values) 
 
     def __str__(self):
         """Return a string representation of the Ngon."""
@@ -30,76 +28,63 @@ class NGon(object):
         return ''.join(str(x) for x in chain.from_iterable(
             self[(i + start_ix) % self.n] for i in range(self.n)))
 
+    def __hash__(self):
+        return hash(str(self))
+
     def __getitem__(self, key):
-        return (self.values[key][0], self.values[key][1], self.values[(key + 1) % self.n][1])
-
-    def _verify(self):
-        """Verify that the values form a self-consistent solution"""
-        total = sum(self[0])
-
-        # Potential optimization here - check total against max/min possible values
-
-        for i in range(1, self.n):
-            if sum(self.values[i]) != total:
-                return False
-
-        return True
-
-    def fill_values(self, values):
-        """Take an input list of value pairs and generate a magic Ngon,
-        filling the overlapping values in each triplet."""
-        # First rotate the values list until we have the lowest edge value at
-        # the start.
-        min_edge_val = (2 * self.n) + 1
-        for i in range(self.n):
-            if values[i][0] < min_edge_val:
-                min_edge_val = values[i][0]
-                min_edge_ix = i
-
-        values = values[min_edge_ix:] + values[:min_edge_ix]
-
-        # Now fill in the overlapping third element of each triplet
-        for i in range(self.n):
-            for j in range(2):
-                self.values[i][j] = values[i][j]
-            self.values[i - 1][2] = self.values[i][1]
+        return self._values[key]
 
 
 def main():
-    """Generate all possible Ngon solutions. Of those that verify as correct,
-    convert to strings of digits. Print the numerically largest such digit
-    string."""
-    ngon_list = []
-    ngon_int_list = []
+    """Find all possible NGon solutions. Print the solution with the greatest
+    numerical value of ngon digits with the required length."""
+    ngon_sols = find_all_ngon_sols()
+    ngon_strs = set(str(ngon) for ngon in ngon_sols)
+    ngon_ints = set(int(ngon_str) for ngon_str in ngon_strs
+                    if len(ngon_str) == TARGET_LEN) 
 
-    for value_set in iter_values(range(1, (2 * N) + 1)):
-        ngon = NGon(N, value_set)
-        if ngon.valid:
-            ngon_list.append(ngon)
-
-    for ngon in ngon_list:
-        ngon_str = str(ngon)
-        assert len(ngon_str) == 16
-        ngon_int_list.append(int(ngon_str))
-
-    print("Max ngon integer is {}".format(max(ngon_int_list)))
+    print("Max ngon integer is {}".format(max(ngon_ints)))
 
 
-def iter_values(vals):
-    """Iterates through all possible unique orderings of pairings for a list
-    of values. Ignores orderings that are cyclically equivalent."""
-    num_pairs = len(vals) // 2
+def find_all_ngon_sols():
+    """Finds all valid NGon solutions. Returns a set to remove solutions
+    that are cyclically equivalent (i.e. have the same Ngon strings.)"""
+    ngon = [None for _ in range(N)] 
+    ngon_set = set()
+    numbers = set(range(1, (2 * N) + 1))
 
-    for pairing in iter_ordered_pairs(list(vals)):
-        for p in permutations(pairing[1:]):
-            value_list = [pairing[0]] + list(p)
+    for triplet in permutations(numbers, 3):
+        ngon[0] = tuple(triplet)
+        total = sum(triplet)
+        next_ngon_set = set()
+        fill_ngon(ngon, numbers - set(triplet), 1, next_ngon_set, total)
+        ngon_set |= next_ngon_set
 
-            # Optimization: only yield value lists if 10 is the first value
-            # in a pairing, as only these will give 16-digit strings.
-            for i in range(num_pairs):
-                if value_list[i][0] == MUST_BE_IN_EDGE:
-                    yield value_list
-                    break
+    return ngon_set
+
+
+def fill_ngon(ngon, numbers, i, ngon_set, total):
+    """Recurisvely find all valid ngon solutions, adding them to ngon_set."""
+    assert i < N
+    assert len(ngon) == N
+
+    if len(numbers) > 2:
+        for pair in permutations(numbers, 2):
+            next_triplet = (pair[0], ngon[i - 1][2], pair[1])
+            if sum(next_triplet) == total:
+                ngon[i] = next_triplet
+                fill_ngon(ngon, numbers - set(pair), i + 1, ngon_set, total)
+                ngon[i] = None
+    else:
+        assert len(numbers) == 1
+        next_triplet = ((numbers.pop()), ngon[i - 1][2], ngon[0][1])
+        if sum(next_triplet) == total:
+            ngon[i] = next_triplet
+            assert i == N - 1
+            assert len(ngon) == N 
+            for i in range(N):
+                assert sum(ngon[i]) == total
+            ngon_set.add(NGon(ngon))
 
 
 if __name__ == '__main__':
